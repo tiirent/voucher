f5b9d204c6cce2cf0b37aa8ebdc5e01f8f778fd9
Trent <nguy2284@umn.edu>
2025-04-17 01:28:27 -0500

    init navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import AppNavbar from '@/components/AppNavbar.vue'
 </script>
 
 <template>
   <v-layout>
-    <v-main>
-      <RouterView />
+    <AppNavbar />
+    <v-main class="d-flex align-start">
+      <v-container class="pa-0">
+        <RouterView />
+      </v-container>
     </v-main>
   </v-layout>
 </template>
 
 <style>
+.v-main {
+  min-height: fit-content !important;
+}
 </style>
---
+++ client/src/components/AppNavbar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// Component for the main navigation bar
+</script>
+
+<template>
+  <v-app-bar flat elevation="0" color="transparent">
+    <v-app-bar-title>4u</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-btn
+      variant="outlined"
+      color="#64A9AF"
+      class="mr-2"
+      density="comfortable"
+    >
+      Login
+    </v-btn>
+    <v-btn
+      variant="outlined"
+      color="#64A9AF"
+      density="comfortable"
+    >
+      Signup
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<style scoped>
+.v-app-bar-title {
+  color: #64A9AF;
+}
+
+</style> 
